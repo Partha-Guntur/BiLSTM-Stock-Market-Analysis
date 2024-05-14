4b5cb702789844b5fee98098fc88940a376bf4d9
partha <parthagunturu2003@gmail.com>
2024-05-14 18:35:23 +0530

    Hello

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>This is a Stock Trading Bot which can, predict the future price value per a time on a graph</h1>
+      <h4>And this webpage would show the Balance sheets, ROI, E/S ratio, P/E ratio, Return on Net worth, Volume trading, Total Assets, Return on Equity</h4>
+    </div>
+  )
+}
+export default App;
