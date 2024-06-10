996a8a98674f8678dc3d9693cf1c089dbc45f789
partha <parthagunturu2003@gmail.com>
2024-06-10 10:43:40 +0530

    Commit

---
+++ nifty50-dashboard/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+      <h2>Nifty50 Analysis</h2>
+      <div>
+        <h3>Closing Prices Over Last 5 Years</h3>
+        <img src='nifty50_closing_prices.png'alt="Nifty50 Closing Prices" />
+      </div><br></br>
+      <div>
+        <h3>Closing Prices with 50-Day and 200-Day Moving Averages</h3>
+        <img src='nifty50_moving_averages.png' alt="Nifty50 Moving Averages" />
+      </div><br></br>
+      <div>
+        <h3>Yearly Returns Over Last 5 Years</h3>
+        <img src='nifty50_yearly_return.png' alt="Nifty50 Yearly Returns" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
