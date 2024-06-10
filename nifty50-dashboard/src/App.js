bad3f8469055bee0b9e12aeb0e9e89b4cba3f35a
partha <parthagunturu2003@gmail.com>
2024-06-10 15:17:03 +0530

    Commit

--- nifty50-dashboard/src/App.js
+++ nifty50-dashboard/src/App.js
@@ -4,24 +4,24 @@ function App() {
 
   return (
     <div>
-
       <h2>Nifty50 Analysis</h2>
       <p>Following are the Representative graphs of previous 5 Year <strong>(from 2019-2024 currently)</strong> Analysis</p>
-      <div>
-        <div>
+      <div className="grid-container">
+        <div className="grid-item">
           <h3>Closing Prices Over Last 5 Years</h3>
           <img src='nifty50_closing_prices.png'alt="Nifty50 Closing Prices" />
         </div><br></br>
-        <div>
+        
+        <div className="grid-item">
           <h3>Closing Prices with 50-Day and 200-Day Moving Averages</h3>
           <img src='nifty50_moving_averages.png' alt="Nifty50 Moving Averages" />
         </div><br></br>
-        <div>
+        
+        <div className="grid-item">
           <h3>Yearly Returns Over Last 5 Years</h3>
           <img src='nifty50_yearly_return.png' alt="Nifty50 Yearly Returns" />
         </div>
       </div>
-
     </div>
   );
 }
